6266103cd6df348440cf4f8bd2d831893f186212
ransaha <meetranajoysaha@gmail.com>
2019-02-21 03:59:28 +0530

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from django.shortcuts import render,redirect
+import os
+from django.db import connection
 
+def home(request):
+      return render(request, 'home.html')
+  
 # Create your views here.
