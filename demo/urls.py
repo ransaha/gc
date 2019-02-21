ba4db6d419738561e6de92631b8c1ca44f55229a
ransaha <meetranajoysaha@gmail.com>
2019-02-21 11:27:07 +0530

    Update urls.py

--- demo/urls.py
+++ demo/urls.py
@@ -12,9 +12,10 @@ Class-based views
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from mysite.views import *
 
 urlpatterns = [
     # Examples:
-    url(r'^$', 'mysite.views.home', name='home'),
+    url(r'^$',home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
