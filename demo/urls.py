1babd970a0710d4f9ebe41628afde6227a603e17
ransaha <meetranajoysaha@gmail.com>
2019-02-21 10:13:29 +0530

    Update urls.py

--- demo/urls.py
+++ demo/urls.py
@@ -8,15 +8,13 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+    """
+
+    
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns(
-   url(r'^$', 'mysite.view.home', name = 'home'),
+   url(r'^$', 'mysite.views.home', name = 'home'),
    url(r'^admin', include(admin.site.urls)),
 )
